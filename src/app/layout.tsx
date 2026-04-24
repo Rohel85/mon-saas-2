ff5379f96ae93ba51c75dcce1dd161a1ae20eeba
Martin <martincraipeau@gmail.com>
2026-04-24 02:25:58 +0200

    fix providers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import { Toaster } from "sonner"
-import { Providers } from "@/components/layout/Providers"
+import Providers from "@/components/layout/Providers"
 import "./globals.css"
 
 const inter = Inter({ subsets: ["latin"] })
